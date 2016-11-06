c25402d1b590cf8b17128891df4301faf7e1c953
Daniel Derhami <daniel.derhami@gmail.com>
2016-11-06 01:03:59 +0000

    Soft17HitStrategy added (model/rules)

---
+++ model/rules/Soft17HitStrategy .java
@@ -0,0 +1,35 @@
+package black_jack.model.rules;
+
+import black_jack.model.Card;
+import black_jack.model.Player;
+
+public class Soft17HitStrategy implements IHitStrategy {
+
+    private final int g_hitLimit = 17;
+
+    @Override
+    public boolean DoHit(Player a_dealer) {
+        
+        Card c = a_dealer.nextCard;
+        
+        if ((a_dealer.CalcScore() + c.GetValue().ordinal()) < g_hitLimit) {
+            return true;
+        } else {
+            if (c.GetValue().ordinal() == 10) {
+                boolean flag = false;
+                for (Card card : a_dealer.GetHand()) {
+                    if (card.GetValue() == Card.Value.Ace) {
+                        flag = true;
+                        break;
+                    }
+                }
+                if (flag && a_dealer.CalcScore() == g_hitLimit) {
+                    return true;
+                }
+            } else {
+                return false;
+            }
+        }
+        return false;
+    }
+}
