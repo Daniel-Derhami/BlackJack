fb78373d22b60660616593ae199da67c6665d906
Daniel Derhami <daniel.derhami@gmail.com>
2016-11-06 00:51:31 +0000

    Package names corrected (IView.java)

--- view/IView.java
+++ view/IView.java
@@ -5,7 +5,7 @@ public interface IView
   void DisplayWelcomeMessage();
   int GetInput();
   void DisplayCard(BlackJack.model.Card a_card);
-  void DisplayPlayerHand(Iterable<BlackJack.model.Card> a_hand, int a_score);
-  void DisplayDealerHand(Iterable<BlackJack.model.Card> a_hand, int a_score);
+  void DisplayPlayerHand(Iterable<black_jack.model.Card> a_hand, int a_score);
+  void DisplayDealerHand(Iterable<black_jack.model.Card> a_hand, int a_score);
   void DisplayGameOver(boolean a_dealerIsWinner);
 }
