e571536a8bb6a13c295877589c7d0c00b6c68189
Daniel Derhami <daniel.derhami@gmail.com>
2016-11-08 00:17:52 +0000

    playgame

--- controller/PlayGame.java
+++ controller/PlayGame.java
@@ -9,10 +9,44 @@ public class PlayGame extends Observer {
     private Game a_game;
     private IView a_view;
 
-    public PlayGame(Game a_game, IView a_view) {}
+    public PlayGame(Game a_game, IView a_view) {
+        this.a_game = a_game;
+        this.a_view = a_view;
+        this.a_game.attach(this);
+        a_view.DisplayWelcomeMessage();
 
-    public boolean Play() {}
+        a_view.DisplayDealerHand(a_game.GetDealerHand(), a_game.GetDealerScore());
+        a_view.DisplayPlayerHand(a_game.GetPlayerHand(), a_game.GetPlayerScore());
+    }
+
+    public boolean Play() {
+
+        int input = a_view.GetInput();
+        if (input == 'p') {
+            a_game.NewGame();
+        } else if (input == 'h') {
+            a_game.Hit();
+        } else if (input == 's') {
+            a_game.Stand();
+        }
+
+        return input != 'q';
+    }
 
     @Override
-    public void update() {}
+    public void update() {
+        try {
+            Thread.sleep(5000);
+            a_view.DisplayWelcomeMessage();
+
+            a_view.DisplayDealerHand(a_game.GetDealerHand(), a_game.GetDealerScore());
+            a_view.DisplayPlayerHand(a_game.GetPlayerHand(), a_game.GetPlayerScore());
+
+            if (a_game.IsGameOver()) {
+                a_view.DisplayGameOver(a_game.IsDealerWinner());
+            }
+
+        } catch (Exception ex) {
+        }
+    }
 }
