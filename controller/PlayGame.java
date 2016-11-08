a3547fe41f6b0e0c113cc11236e5202f9ba68a84
Daniel Derhami <daniel.derhami@gmail.com>
2016-11-08 00:12:28 +0000

    playgame

--- controller/PlayGame.java
+++ controller/PlayGame.java
@@ -4,4 +4,15 @@ import black_jack.view.IView;
 import black_jack.model.Game;
 import black_jack.view.Observer;
 
-public class PlayGame extends Observer {}
+public class PlayGame extends Observer {
+
+    private Game a_game;
+    private IView a_view;
+
+    public PlayGame(Game a_game, IView a_view) {}
+
+    public boolean Play() {}
+
+    @Override
+    public void update() {}
+}
